80b797fb97f81a35f9f314c265283e1127f391e0
Nick Xitco <nxitco@gmail.com>
2025-05-25 19:48:34 -0400

    ip display

---
+++ custom-displays/ip-display.cc
@@ -0,0 +1,107 @@
+#include "led-matrix.h"
+#include "graphics.h"
+
+#include <unistd.h>
+#include <math.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+using namespace rgb_matrix;
+
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo) {
+  interrupt_received = true;
+}
+
+// Function to get the first non-localhost IP address
+std::string GetIPAddress() {
+    struct ifaddrs *ifaddr, *ifa;
+    std::string ip = "No IP";
+    
+    if (getifaddrs(&ifaddr) == -1) {
+        return "Error";
+    }
+
+    // Walk through linked list of interfaces
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        // Check if it's an IPv4 address
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            // Skip localhost
+            if (strcmp(ifa->ifa_name, "lo") == 0)
+                continue;
+
+            char ipstr[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &((struct sockaddr_in *)ifa->ifa_addr)->sin_addr,
+                     ipstr, INET_ADDRSTRLEN);
+            ip = ipstr;
+            break;
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return ip;
+}
+
+int main(int argc, char *argv[]) {
+  RGBMatrix::Options defaults;
+  defaults.hardware_mapping = "regular";
+  defaults.rows = 32;
+  defaults.cols = 64;
+  defaults.chain_length = 1;
+  defaults.parallel = 1;
+  defaults.show_refresh_rate = true;
+
+  // Set up signal handler for clean exit
+  signal(SIGTERM, InterruptHandler);
+  signal(SIGINT, InterruptHandler);
+
+  // Create the RGB matrix
+  RGBMatrix *matrix = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+  if (matrix == NULL)
+    return 1;
+
+  // Create a new canvas to be able to do double buffering
+  FrameCanvas *offscreen = matrix->CreateFrameCanvas();
+
+  // Create a font
+  rgb_matrix::Font font;
+  if (!font.LoadFont("../fonts/7x13.bdf")) {
+    fprintf(stderr, "Couldn't load font\n");
+    return 1;
+  }
+
+  // Colors
+  Color ip_color(0, 255, 0);    // Green for IP
+  Color label_color(255, 255, 0); // Yellow for label
+
+  while (!interrupt_received) {
+    // Clear the offscreen canvas
+    offscreen->Clear();
+
+    // Get current IP
+    std::string ip = GetIPAddress();
+
+    // Draw "IP:" label
+    rgb_matrix::DrawText(offscreen, font, 5, 15, label_color, "IP:");
+
+    // Draw the IP address
+    rgb_matrix::DrawText(offscreen, font, 5, 30, ip_color, ip.c_str());
+
+    // Swap the offscreen canvas with the onscreen one
+    offscreen = matrix->SwapOnVSync(offscreen);
+
+    // Update every 5 seconds
+    sleep(5);
+  }
+
+  // Clean up
+  delete matrix;
+  return 0;
+} 
